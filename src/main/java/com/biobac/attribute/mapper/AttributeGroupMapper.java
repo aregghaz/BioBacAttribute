1d88be07a875b70fbe7f36dcd7237b274e6bdc89
Alik Sargsyan <alik.sargsyan@coderepablic.am>
2025-12-03 16:28:37 +0400

    modified some logic and fixed bugs

--- src/main/java/com/biobac/attribute/mapper/AttributeGroupMapper.java
+++ src/main/java/com/biobac/attribute/mapper/AttributeGroupMapper.java
@@ -9,14 +9,28 @@ import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Mapper(componentModel = "spring")
 public interface AttributeGroupMapper {
     @Mapping(source = "definitions", target = "attributes")
     AttributeGroupResponse toDto(AttributeGroup entity);
 
+    @Mapping(target = "attributeGroupIds", expression = "java(mapGroupIds(entity))")
     AttributeDefResponse toDto(AttributeDefinition entity);
 
     AttributeGroup toEntity(AttributeGroupRequest request);
 
     void update(@MappingTarget AttributeGroup entity, AttributeGroupRequest request);
+
+    default List<Long> mapGroupIds(AttributeDefinition entity) {
+        if (entity.getGroups() == null) {
+            return new ArrayList<>();
+        }
+        return entity.getGroups().stream()
+                .map(AttributeGroup::getId)
+                .collect(Collectors.toList());
+    }
 }
